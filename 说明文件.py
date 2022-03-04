ac66ba8c01e633db91933d906105609feec7a404
Jf-Chen <485133380@qq.com>
2022-03-04 09:47:26 +0800

    Initial commit

---
+++ 说明文件.py
@@ -0,0 +1,17 @@
+datasets.tar
+    cifar_fs      
+    FC100	    
+    miniimagenetimages	
+        images	test.csv  train.csv  validation.csv
+            images下都是图片
+    tieredimagenet
+    CUB_200_2011  
+    miniimagenet   
+        test.pkl  
+            data,labels 12000,12000
+        train.pkl  
+            data,labels 38400,38400
+        validation.pkl
+            data,labels  9600, 9600
+        
+    omniglot
